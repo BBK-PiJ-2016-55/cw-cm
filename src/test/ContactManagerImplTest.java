e1489586df8da2180f5ffb04cc6622bbaaed051f
essvee <svince04@mail.bbk.ac.uk>
2017-02-18 19:02:32 +0000

    adds uniqueness tests for addContact()

---
+++ src/test/ContactManagerImplTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import impl.ContactManagerImpl;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by svince04 on 18/02/2017 for cw-cm.
+ */
+public class ContactManagerImplTest {
+   private ContactManagerImpl conManImp;
+
+   @Before
+   public void startUp() {
+       conManImp = new ContactManagerImpl();
+   }
+
+   @Test (expected = IllegalArgumentException.class)
+   public void testAddNewContactEmotyName() {
+       int conId = conManImp.addNewContact("Joey", "");
+   }
+
+   @Test
+   public void testAddNewContact() {
+        int conId = conManImp.addNewContact("Joey", "A beautiful idiot. ");
+        assertEquals(3, conId);
+    }
+
+    @Test
+    public void testAddNewContactUniqueId() {
+        int joeyId = conManImp.addNewContact("Joey", "A beautiful idiot. ");
+        int kangaId = conManImp.addNewContact("Kanga", "A bigger version of Joey. ");
+        assertFalse(joeyId == kangaId);
+    }
+
+
+}
