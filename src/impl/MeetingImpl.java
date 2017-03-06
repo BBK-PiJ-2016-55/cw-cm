93107ed302bd821920a3ab85a0bdfa308b34117d
essvee <svince04@mail.bbk.ac.uk>
2017-03-06 00:04:16 +0000

    changes constructor access modifier to package-private

--- src/impl/MeetingImpl.java
+++ src/impl/MeetingImpl.java
@@ -4,7 +4,6 @@ import spec.Contact;
 import spec.Meeting;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.Set;
 
 /**
@@ -15,7 +14,7 @@ public abstract class MeetingImpl implements Meeting {
     private Calendar date;
     private Set<Contact> attendees;
 
-    public MeetingImpl(int id, Calendar date, Set<Contact> attendees) throws IllegalArgumentException, NullPointerException {
+    MeetingImpl(int id, Calendar date, Set<Contact> attendees) throws IllegalArgumentException, NullPointerException {
         if (id <= 0) {
             throw new IllegalArgumentException("ID cannot be negative");
         } else if (date == null || attendees == null) {
