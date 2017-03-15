a9d4894fa6a0dff1b45bbf512b17df5f79ae3c42
essvee <svince04@mail.bbk.ac.uk>
2017-03-15 13:26:03 +0000

    removes checkstyle warnings and makes tests more efficient

--- src/test/java/test/PastMeetingImplTest.java
+++ src/test/java/test/PastMeetingImplTest.java
@@ -1,7 +1,5 @@
 package test.java.test;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
@@ -18,17 +16,13 @@ import org.junit.Test;
  * PiJ Coursework 3.
  */
 public class PastMeetingImplTest {
-  private Calendar pastDate;
+  private Calendar pastDate = new GregorianCalendar(2012, 4, 5, 11, 30);
+  private Contact mrEd = new ContactImpl(1, "Mr. Ed", "Tall and not very bright.");
   private final Set<Contact> attendeeSet = new HashSet<>();
-  private PastMeetingImpl pastMeeting;
 
   @Before
   public void setUp() {
-    Contact contact = new ContactImpl(1, "Pluto", "Tall and not very bright.");
-    attendeeSet.add(contact);
-    pastDate = new GregorianCalendar(2012, 4, 5, 11, 30);
-    pastMeeting = new PastMeetingImpl(1, pastDate, attendeeSet,
-            "I shouldn't have gone into the dog park.");
+    attendeeSet.add(mrEd);
   }
 
   @Test (expected = IllegalArgumentException.class)
@@ -56,11 +50,4 @@ public class PastMeetingImplTest {
     attendeeSet.clear();
     new PastMeetingImpl(2, pastDate, attendeeSet, "Contact set is empty.");
   }
-
-  // getContacts tests
-  @Test
-  public void testGetNotes() {
-    assertEquals("I shouldn't have gone into the dog park.", pastMeeting.getNotes());
-  }
-
 }
